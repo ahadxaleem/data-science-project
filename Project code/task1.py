2cbee3542e3c67fa12aca0006743bb3e7574e884
Ahad <ahadsaleem306@gmail.com>
2022-04-09 12:59:55 +0500

    completed first task

--- Project code/task1.py
+++ Project code/task1.py
@@ -1,11 +1,23 @@
 import os
 import json
 def task1():
-    onlyfiles = next(os.walk(r"C:\Users\bbb\Desktop\proj1\course\data\a1\content\HealthStory"))[2] #dir is your directory path as string
+    onlyfiles = next(os.walk(r"C:\Users\bbb\Desktop\proj1\course\data\a1\content\HealthStory"))[2]
     x=len(onlyfiles)
     # Opening JSON file
     f = open(r'C:\Users\bbb\Desktop\proj1\course\data\a1\reviews\HealthStory.json')
     data=json.load(f)
     y=len(data)
-    
+    z=0
+    f2=open(r'C:\Users\bbb\Desktop\proj1\course\data\a1\engagements\HealthStory.json')
+    data2=json.load(f2)
+    for i in data2.values():
+        z+=len(i['tweets'])
+        # print(i)
+    dictionary={ 
+    "Total number of articles": x, 
+    "Total number of reviews": y,  
+    "Total number of tweets": z 
+    }
+    with open("task1.json", "w") as outfile:
+        json.dump(dictionary, outfile) 
     return
