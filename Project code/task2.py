86291e7d8220ca1d1a6aa45ccd79d164bd6c837f
Ahad <ahadsaleem306@gmail.com>
2022-04-12 18:24:36 +0500

    corrected task2 for the files that shouldn't be included in the output csv file

--- Project code/task2.py
+++ Project code/task2.py
@@ -1,6 +1,15 @@
 import csv
 import json
+import os
+from tabnanny import filename_only
 def task2():
+    # assign directory
+    directory = r'C:\Users\bbb\Desktop\proj1\course\data\a1\content\HealthStory'
+    news_id=[]
+    for filename in os.scandir(directory):
+            if filename.is_file():
+                news_id.append(filename.name[:19])
+
     # field names 
     fields = ['news_id', 'news_title', 'review_title', 'rating', 'num_satisfactory']
     # Opening JSON file
@@ -16,16 +25,17 @@ def task2():
         #writing all the rows
         for i in data:
             row=[]
-            row.append(i['news_id'])
-            row.append(i['original_title'])
-            row.append(i['title'])
-            row.append(str(i['rating']))
-            num_satisfactory=0
-            for x in i['criteria']:
-                if x['answer']=='Satisfactory':
-                    num_satisfactory+=1
-            row.append(str(num_satisfactory))
-            # writing the data rows 
-            csvwriter.writerow(row)
+            if i['news_id'] in news_id:
+                row.append(i['news_id'])
+                row.append(i['original_title'])
+                row.append(i['title'])
+                row.append(str(i['rating']))
+                num_satisfactory=0
+                for x in i['criteria']:
+                    if x['answer']=='Satisfactory':
+                        num_satisfactory+=1
+                row.append(str(num_satisfactory))
+                # writing the data rows 
+                csvwriter.writerow(row)
     
     return
